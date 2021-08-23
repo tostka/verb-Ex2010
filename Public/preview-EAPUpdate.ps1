82c7ed0b76c93b8d4064cfb27e2e58e6f68d517f
Tostka <tostka@users.noreply.github.com>
2021-08-23 15:33:44 -0500

    Update preview-EAPUpdate.ps1
    * 1:11 PM 8/23/2021 fixed bug in regex ps replace; haven't ested balance of fancy substring replace options; %g, %s working.
            * 2:00 PM 8/23/2021 added drop of illegal chars (shows up distinctively as spaces in dname :P); fixed bug in regex ps replace; haven't ested balance of fancy substring replace options; %g, %s working.

--- Public/preview-EAPUpdate.ps1
+++ Public/preview-EAPUpdate.ps1
@@ -18,9 +18,12 @@
         AddedWebsite: URL
         AddedTwitter: URL
         REVISIONS
-        * 1:11 PM 8/23/2021 fixed bug in regex ps replace; haven't ested balance of fancy substring replace options; %g, %s working.
+        * 2:00 PM 8/23/2021 added drop of illegal chars (shows up distinctively as spaces in dname :P); fixed bug in regex ps replace; haven't ested balance of fancy substring replace options; %g, %s working.
         .DESCRIPTION
         preview-EAPUpdate.ps1 - Code to approximate EmailAddressTemplate-generated email addresses
+        Note: This is a quick & dirty *approximation* of the generated email address. Doesn't support multiple %rxy replaces on mult format codes. Just does the first one; plus any non-replace %d|s|i|m|g's. 
+        Don't rely on this, it's just intended to quickly confirm assigned primarysmtpaddress roughly matches the intended EAP template.
+        If it doesn't, put eyes on it and confirm, don't use this to drive any revision of the email address!
 
         Latest specs:[Email address policies in Exchange Server | Microsoft Docs - docs.microsoft.com/](https://docs.microsoft.com/en-us/exchange/email-addresses-and-address-books/email-address-policies/email-address-policies?view=exchserver-2019)
 
@@ -85,14 +88,17 @@
         PS> preview-EAPUpdate  -eap $eaps[16] -Recipient $trcp -verbose ;
         Preview specified recipient using the specified EAP (17th in the set in the $eaps variable).
         .EXAMPLE
-        PS> preview-EAPUpdate  -addresstemplate '%3g%5s@contoso.com' -Recipient $trcp -verbose ;
-        Preview target recipient using a template that takes givename[0-3]surname[0-5]@contoso.com
+        PS> preview-EAPUpdate  -addresstemplate '%5s%3g@contoso.com' -Recipient $trcp -verbose ;
+        Preview target recipient using a template that takes surname[0-4]givename[0-3]@contoso.com
         .EXAMPLE
-        PS> preview-EAPUpdate  -addresstemplate '%1g%s@microsoft.com' -Recipient $trcp -verbose ;
-        Preview target recipient using %1g%s@microsoft.com|JWilson@microsoft.com|"[1stcharGivenName][surname]@domain"|
+        PS> preview-EAPUpdate  -addresstemplate '%5s%1g%1i@contoso.com' -Recipient $trcp -verbose ;
+        Preview target recipient using a template that takes surname[0-4]givename[0]mi[0]@contoso.com
         .EXAMPLE
-        PS> preview-EAPUpdate  -addresstemplate '%r._%d@microsoft.com' -Recipient $trcp -verbose ;
-        Preview target recipient using %r._%d@microsoft.com|JamesC_Wilson@microsoft.com|"[replace periods in displayname with underscore]@domain"|
+        PS> preview-EAPUpdate  -addresstemplate '%1g%s@contoso.com' -Recipient $trcp -verbose ;
+        Preview target recipient using %1g%s@contoso.com|JWilson@contoso.com|"[1stcharGivenName][surname]@domain"|
+        .EXAMPLE
+        PS> preview-EAPUpdate  -addresstemplate '%r._%d@contoso.com' -Recipient $trcp -verbose ;
+        Preview target recipient using %r._%d@contoso.com|JamesC_Wilson@contoso.com|"[replace periods in displayname with underscore]@domain"|
         .EXAMPLE
         PS> preview-EAPUpdate  -addresstemplate "%g.%r''%s@domain.co" -Recipient $trcp -verbose ;
         Preview target recipient using %g.%r''%s@domain.com|James.Wilson@domain|"[givenname].[surname,delete all APOSTROPHES]@domain"|
@@ -102,6 +108,14 @@
         .EXAMPLE
         PS> preview-EAPUpdate  -addresstemplate ' %d@contoso.com' -Recipient $trcp -verbose ;
         Preview target recipient using %d@domain.com|JamesCWilson@domain.com|"Displayname@domain"
+        .EXAMPLE
+        PS> $genEml = preview-EAPUpdate  -addresstemplate ' %d@contoso.com' -Recipient $trcp -verbose ;
+            if(($geneml -ne $trcp.primarysmtpaddress)){
+                write-warning "Specified recip's PrimarySmtpAddress does *not* appear to match specified template!`nmanualy *review* the template specs`nand validate that the desired scheme is being applied!"
+            }else {
+                "PrimarysmtpAddr $($trcp.primarysmtpaddress) roughly conforms to specified template primary addr..."
+            } ;
+        Example testing output against $trcp primarySmtpAddress.
         .LINK
         https://github.com/tostka/verb-ex2010
         .LINK
@@ -132,6 +146,7 @@
         BEGIN { 
             # function self-name (equiv to script's: $MyInvocation.MyCommand.Path) ;
             ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
+            $rgxEmailDirLegalChars = "[0-9a-zA-Z-._+&']" ; 
             reconnect-ex2010 -verbose:$false ; 
     
         } ;  # BEGIN-E
@@ -164,53 +179,127 @@
                 Break #Opts: STOP(debug)|EXIT(close)|CONTINUE(move on in loop cycle)|BREAK(exit loop iteration)|THROW $_/'CustomMsg'(end script with Err output)
             } ; 
             $genAddr = $null ; 
-            if($ptmpl -match '^@.*m$'){$genAddr = $ptmpl = $ptmpl.replace('@',"$($Recipient.alias)@")} ;
-            if($ptmpl.contains('%g')){$genAddr = $ptmpl = $ptmpl.replace('%g',$usr.firstname)} ;
-            if($ptmpl.contains('%s')){$genAddr = $ptmpl = $ptmpl.replace('%s',$usr.lastname)} ;
-            if($ptmpl.contains('%d')){$genAddr = $ptmpl = $ptmpl.replace('%d',$usr.displayname)} ;
-            if($ptmpl.contains('%i')){$genAddr = $ptmpl = $ptmpl.replace('%i',$usr.Initials)} ;
-            if($ptmpl.contains('%m')){$genAddr = $ptmpl = $ptmpl.replace('%m',$Recipient.alias)} ;
-            if($ptmpl -match '(%(\d)g)'){
-                $ltrs = $matches[2] ; 
-                #$genAddr = $ptmpl = $ptmpl -replace "(%\dg)","$1,$($usr.firstname.substring(0,$ltrs))" ;
-                $genAddr = $ptmpl = $ptmpl -replace "(%\dg)","$($usr.firstname.substring(0,$ltrs))"
-            } ; 
-            if($ptmpl -match "(%(\d)s)"){
-                $ltrs = $matches[2] ; 
-                #$genAddr = $ptmpl = $ptmpl -replace "(%\ds)","$1,$($usr.lastname.substring(0,$ltrs))"
-                $genAddr = $ptmpl =$ptmpl -replace "(%\ds)","$($usr.lastname.substring(0,$ltrs))" ;
-            } ; 
-            if($ptmplZ -match "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])%(d|g|i|m|s)@.*$"){
+            # [0-9a-zA-Z-._+&]{1,64} # alias rgx legal
+            # [0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+
+            # dirname
+            # ^([0-9a-zA-Z]+[-._+&'])*[0-9a-zA-Z]+ 
+            # @domain: @([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$
+
+            #if($ptmpl -match '^@.*$'){$genAddr = $ptmpl = $ptmpl.replace('@',"$($Recipient.alias)@")} ;
+            if($ptmpl -match '^@.*$'){
+                write-verbose "(matched Alias@domain)" ;
+                $string = ($Recipient.alias.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl.replace('@',"$($string)@")
+            } ;
+            # do replace first, as the %d etc are simpler matches and don't handle the leading %r properly.
+            if($ptmpl -match "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])%(d|g|i|m|s)@.*$"){
                 $x = $matches[1] ; 
                 $y = $matches[2] ; 
                 $vari = $matches[3] ; 
-                write-host "Parsed:`nx:$($x)`ny:$($y)`nvari:$($vari)" ;
+                write-verbose "Parsed:`nx:$($x)`ny:$($y)`nvari:$($vari)" ;
                 switch ($vari){
-                  'd' {
-                      $ptmplZ = $ptmpl.replace("%d",$usr.displayname.replace($x,$y)) ;
-                  }
-                  'g'  {
-                      $ptmplZ = $ptmpl.replace("%g",$usr.firstname.replace($x,$y)) ;
-                  }
-                  'i' {
-                      $ptmplZ = $ptmpl.replace("%i",$usr.Initials.replace($x,$y)) ;
-                  }
-                  'm' {
-                      $ptmplZ = $ptmpl.replace("%m",$Recipient.alias.replace($x,$y)) ;
-                  }
-                  's' {
-                      $ptmplZ = $ptmpl.replace("%s",$usr.lastname.replace($x,$y)) ;
-                  }
-                  default {
-                      throw "unrecognized template: replace (%r) character with no targeted variable (%(d|g|i|m|s))" ;
-                  }
+                    'd' {
+                        write-verbose "(matched replace $($x) w $($y) on displayname)" ;
+                        #$ptmpl = $ptmpl.replace("%d",$usr.displayname.replace($x,$y)) ;
+                        $string = ($usr.displayname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                        #$genAddr = $ptmpl = $ptmpl = $ptmpl.replace("%d",$string.replace($x,$y))
+                        # subout %rxy first, then the trailing %d w name
+                        $genAddr = $ptmpl = $ptmpl = $ptmpl -replace "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])",'' -replace "%d",$string.replace($x,$y)
+                    }
+                    'g'  {
+                        write-verbose "(matched replace $($x) w $($y) on givenname)" ;
+                        #$ptmpl = $ptmpl.replace("%g",$usr.firstname.replace($x,$y)) ;
+                        $string = ($usr.firstname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                        #$genAddr = $ptmpl = $ptmpl = $ptmpl.replace("%g",$string.replace($x,$y))
+                        $genAddr = $ptmpl = $ptmpl = $ptmpl -replace "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])",'' -replace "%g",$string.replace($x,$y)
+                    }
+                    'i' {
+                        write-verbose "(matched replace $($x) w $($y) on initials)" ;
+                        #$ptmpl = $ptmpl.replace("%i",$usr.Initials.replace($x,$y)) ;
+                        $string = ($usr.Initials.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                        #$genAddr = $ptmpl = $ptmpl = $ptmpl.replace("%i",$string.replace($x,$y))
+                        $genAddr = $ptmpl = $ptmpl = $ptmpl -replace "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])",'' -replace "%i",$string.replace($x,$y)
+                    }
+                    'm' {
+                        write-verbose "(matched replace $($x) w $($y) on alias)" ;
+                        #$ptmpl = $ptmpl.replace("%m",$Recipient.alias.replace($x,$y)) ;
+                        $string = ($Recipient.alias.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                        #$genAddr = $ptmpl = $ptmpl = $ptmpl.replace("%m",$string.replace($x,$y))
+                        $genAddr = $ptmpl = $ptmpl = $ptmpl -replace "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])",'' -replace "%m",$string.replace($x,$y)
+                    }
+                    's' {
+                        write-verbose "(matched replace $($x) w $($y) on surname)" ;
+                        #$ptmpl = $ptmpl.replace("%s",$usr.lastname.replace($x,$y)) ;
+                        $string = ($usr.lastname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                        #$genAddr = $ptmpl = $ptmpl = $ptmpl.replace("%s",$string.replace($x,$y))
+                        $genAddr = $ptmpl = $ptmpl = $ptmpl -replace "%r([a-zA-Z_0-9-._+&])([a-zA-Z_0-9-._+&])",'' -replace "%s",$string.replace($x,$y)
+                    }
+                    default {
+                        throw "unrecognized template: replace (%r) character with no targeted variable (%(d|g|i|m|s))" ;
+                    }
                 } ;
+            } ; 
+            if($ptmpl.contains('%g')){
+                write-verbose "(matched %g:displayname)" ;
+                #$genAddr = $ptmpl = $ptmpl.replace('%g',$usr.firstname)
+                $string = ($usr.firstname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl.replace('%g',$string)
+            } ;
+            if($ptmpl.contains('%s')){
+                write-verbose "(matched %s:surname)" ;
+                #$genAddr = $ptmpl = $ptmpl.replace('%s',$usr.lastname)} ;
+                $string = ($usr.lastname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl.replace('%s',$string)
+            };
+            if($ptmpl.contains('%d')){
+                write-verbose "(matched %d:displayname)" ;
+                #$genAddr = $ptmpl = $ptmpl.replace('%d',$usr.displayname)
+                $string = ($usr.displayname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl.replace('%d',$string)
+            } ;
+            if($ptmpl.contains('%i')){
+                write-verbose "(matched %i:initials)" ;
+                #$genAddr = $ptmpl = $ptmpl.replace('%i',$usr.Initials)} ;
+                $string = ($usr.Initials.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl.replace('%i',$string)
+            } ;
+            if($ptmpl.contains('%m')){
+                write-verbose "(matched %m:alias)" ;
+                #$genAddr = $ptmpl = $ptmpl.replace('%m',$Recipient.alias)} ;
+                $string = ($Recipient.alias.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl.replace('%m',$string)
+            } ; 
+            if($ptmpl -match '(%(\d)g)'){
+                $ltrs = $matches[2] ; 
+                write-verbose "(matched %g:displayname, first $($ltrs) chars)" ;
+                #$genAddr = $ptmpl = $ptmpl -replace "(%\dg)","$1,$($usr.firstname.substring(0,$ltrs))" ;
+                #$genAddr = $ptmpl = $ptmpl -replace "(%\dg)","$($usr.firstname.substring(0,$ltrs))"
+                $string = ($usr.firstname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl -replace "(%\dg)","$($string.substring(0,$ltrs))"
+            } ; 
+            if($ptmpl -match "(%(\d)s)"){
+                $ltrs = $matches[2] ; 
+                write-verbose "(matched %s:surname, first $($ltrs) chars)" ;
+                #$genAddr = $ptmpl = $ptmpl -replace "(%\ds)","$1,$($usr.lastname.substring(0,$ltrs))"
+                #$genAddr = $ptmpl =$ptmpl -replace "(%\ds)","$($usr.lastname.substring(0,$ltrs))" ;
+                $string = ($usr.lastname.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl -replace "(%\ds)","$($string.substring(0,$ltrs))"
+            } ; 
+            if($ptmpl -match "(%(\d)i)"){
+                $ltrs = $matches[2] ; 
+                write-verbose "(matched %i:initials, first $($ltrs) chars)" ;
+                #$genAddr = $ptmpl = $ptmpl -replace "(%\ds)","$1,$($usr.lastname.substring(0,$ltrs))"
+                #$genAddr = $ptmpl =$ptmpl -replace "(%\di)","$($usr.initials.substring(0,$ltrs))" ;
+                $string = ($usr.initials.ToCharArray() |?{$_ -match $rgxEmailDirLegalChars})  -join '' ;
+                $genAddr = $ptmpl = $ptmpl -replace "(%\di)","$($string.substring(0,$ltrs))"
             } ; 
 
         } ;  # PROC-E
         END {
-            if($genAddr){$genAddr| write-output }
-            else {
+            if($genAddr){
+                write-verbose "returning generated address:$($genAddr)" ; 
+                $genAddr| write-output 
+            } else {
                 write-warning "Unable to generate a PrimarySmtpAddress model for user" ; 
                 $false | write-output l
             };
